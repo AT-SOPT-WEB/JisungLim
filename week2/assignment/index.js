b9e3e5caee372a52c8804bd2a4f6995f38c61f85
jstar_00 <wltjd6300@naver.com>
2025-04-25 21:11:02 +0900

    [Feat] 2. 상단 섹션(필터링) 기능 구현 완료, createTable 함수로 테이블 생성 로직 분리

--- week2/assignment/index.js
+++ week2/assignment/index.js
@@ -1,5 +1,6 @@
 const STORAGE_KEY_TODOLIST = "todoList";
 const STORAGE_KEY_LISTID = "listId";
+const filterButtons = document.querySelectorAll(".filter");
 const todoInput = document.querySelector(".todo-input");
 const todoPriority = document.querySelector(".todo-priority");
 const addTodoButton = document.querySelector(".addTodo-button");
@@ -10,16 +11,34 @@ const modal = document.querySelector(".modal");
 const closeModalButton = document.querySelector(".closeModal-button");
 const checkedIdSet = new Set(); // 체크한 체크박스의 listId값 저장
 const todoList = JSON.parse(localStorage.getItem(STORAGE_KEY_TODOLIST)) || [];
+// localStorage: 모든 데이터 문자열로 저장
+// JSON.parse: 저장된 문자열을 객체/배열로 바꿔줌
 
 todoList.forEach((todoList, idx) => {
+    createTable(todoList);
+})
+
+filterButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+        const filterType = e.target.dataset.filter;
+        filterTodoList(filterType);
+    })
+})
+addTodoButton.addEventListener("click", addTodo); // hoisting(o)
+completeButton.addEventListener("click", completeTask);
+deleteButton.addEventListener("click", deleteTask);
+closeModalButton.addEventListener("click", closeModal);
+
+function createTable(todo) {
+    // todo: {id, title, complete, priority}
     const tr = document.createElement("tr");
 
     const checkboxTd = document.createElement("td");
-    checkboxTd.className = "table-data"
+    checkboxTd.className = "table-data";
 
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
-    checkbox.dataset.id = todoList.id;
+    checkbox.dataset.id = todo.id;
     checkbox.addEventListener("change", (e) => {
         const id = e.target.dataset.id;
         if (e.target.checked) {
@@ -29,20 +48,32 @@ todoList.forEach((todoList, idx) => {
         }
     });
 
-    const priorityTd = createTd(todoList.priority);
-    const inputTd = createTd(todoList.title);
-    const isCompletedTd = createTd(todoList.completed ? "✅" : "❌");
+    const priorityTd = createTd(todo.priority);
+    const inputTd = createTd(todo.title);
+    const isCompletedTd = createTd(todo.completed ? "✅" : "❌");
 
     checkboxTd.append(checkbox);
     tr.append(checkboxTd, priorityTd, isCompletedTd, inputTd);
     tableBody.append(tr);
-})
+}
 
-addTodoButton.addEventListener("click", addTodo); // hoisting(o)
-completeButton.addEventListener("click", completeTask);
-deleteButton.addEventListener("click", deleteTask);
-closeModalButton.addEventListener("click", closeModal);
+function filterTodoList(selectedFilter) {
+    let filteredList = [];
 
+    if (selectedFilter === "all") {
+        filteredList = todoList;
+    } else if (selectedFilter === "complete") {
+        filteredList = todoList.filter((todo) => todo.completed);
+    } else if (selectedFilter === "incomplete") {
+        filteredList = todoList.filter((todo) => !todo.completed);
+    } else {
+        console.warn("알 수 없는 filter");
+        return;
+    }
+    
+    tableBody.innerHTML = "";
+    filteredList.forEach((todo) => createTable(todo));
+}
 
 function createTd(text) {
     const td = document.createElement("td");
@@ -61,39 +92,17 @@ function addTodo() {
         return;
     }
 
-    // 테이블에 추가
-    const tr = document.createElement("tr");
-
-    const checkboxTd = document.createElement("td");
-    checkboxTd.className = "table-data"
-
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.dataset.id = listId;
-    checkbox.addEventListener("change", (e) => {
-        const id = e.target.dataset.id;
-        if (e.target.checked) {
-            checkedIdSet.add(id);
-        } else {
-            checkedIdSet.delete(id);
-        }
-    });
-    
-    const priorityTd = createTd(priorityValue);
-    const inputTd = createTd(inputValue);
-    const isCompletedTd = createTd("❌");
-
-    checkboxTd.append(checkbox);
-    tr.append(checkboxTd, priorityTd, isCompletedTd, inputTd);
-    tableBody.append(tr);
-
-    // local storage에 추가
     const todo = {
         "id": listId, 
         "title": inputValue, 
         "completed": false, 
         "priority": priorityValue
     };
+
+    // 테이블에 추가
+    createTable(todo);
+
+    // local storage에 추가
     todoList.push(todo);
     localStorage.setItem(STORAGE_KEY_TODOLIST, JSON.stringify(todoList));
     localStorage.setItem(STORAGE_KEY_LISTID, ++listId);
@@ -120,7 +129,7 @@ function completeTask() {
     //         }
     //     }
     // });
-    console.log(checkedIdSet)
+
     for (const id of checkedIdSet) {
         const todo = todoList.find((todo) => {
             return todo.id === Number(id);
