28f727b28ca42a821a153ea4f63f6bbaee8ed46d
jstar_00 <wltjd6300@naver.com>
2025-05-07 22:07:01 +0900

    [Setting] 프로젝트 기본 세팅
    - css library: emotion
    - emotion-reset css 적용
    - globalStyles 파일 선언 및 컴포넌트 전체 적용

---
+++ week4/assignment/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const App = () => {
+  return (
+    <StyledDiv>
+      안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요 안녕하세요
+    </StyledDiv>
+  );
+};
+
+export default App;
+
+const StyledDiv = styled.div`
+  font-size: 2rem;
+`;
